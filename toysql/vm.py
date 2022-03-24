8c9ccfddcede60332d75bba5b05b6997b404cfc9
Craig Mulligan <hey@hobochild.com>
2022-03-24 19:01:11 +0000

    nearly there

--- toysql/vm.py
+++ toysql/vm.py
@@ -1,24 +1,15 @@
-import logging
 from typing import Union
 from toysql.statement import SelectStatement, InsertStatement
+from toysql.table import Table
 
 
 class VM:
     def __init__(self):
-        self.table = {}
+        self.table = Table()
 
     def execute(self, statement: Union[SelectStatement, InsertStatement]):
         if isinstance(statement, SelectStatement):
-            logging.info("this is where we do a select")
-            results = []
-            print(self.table)
-            for id, row in self.table.items():
-                results.append(row)
-
-            return results
+            return self.table.select()
 
         if isinstance(statement, InsertStatement):
-            logging.info("this is where we do a insert")
-            if statement.row:
-                id, name, email = statement.row
-                self.table[id] = (id, name, email)
+            return self.table.insert(statement.row)
