1f50e16eb74ab49f2bbc0e17f41b104f20bf8d1a
Craig Mulligan <hey@hobochild.com>
2022-03-31 14:24:32 +0100

    add bstu

--- toysql/btree.py
+++ toysql/btree.py
@@ -11,25 +11,42 @@ class NodeType:
     leaf = 1
 
 
-class LeafNode:
-    def __init__(self):
+class Node:
+    def __init__(self, page: bytearray):
+        self.page = page
+
+    def __len__(self):
+        return int(len(self.page) + LEAF_NODE_NUM_CELLS_OFFSET)
+
+    def leaf_node_num_cells(self):
+        return len(self.page) + LEAF_NODE_NUM_CELLS_OFFSET
+
+    def leaf_node_cell(self, cell_num):
+        return len(self.page) + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE
+
+    def leaf_node_key(self, cell_num):
+        return self.leaf_node_cell(cell_num)
+
+    def leaf_node_value(self, cell_num):
+        return self.leaf_node_cell(cell_num) + LEAF_NODE_KEY_SIZE
+
+    def initialize_leaf_node(self):
         pass
 
+    def cell_value(self, cell_num):
 
-# +uint32_t* leaf_node_num_cells(void* node) {
-# +  return node + LEAF_NODE_NUM_CELLS_OFFSET;
-# +}
-# +
-# +void* leaf_node_cell(void* node, uint32_t cell_num) {
-# +  return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
-# +}
-# +
-# +uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
-# +  return leaf_node_cell(node, cell_num);
-# +}
-# +
-# +void* leaf_node_value(void* node, uint32_t cell_num) {
-# +  return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
-# +}
-# +
-# +void initialize_leaf_node(void* node) { *leaf_node_num_cells(node) = 0; }
+        pass
+
+    def insert_cell(self, cursor, key, row):
+        num_cells = self.leaf_node_num_cells()
+
+        if num_cells >= LEAF_NODE_MAX_CELLS:
+            raise Exception("Need to implement splitting a leaf node")
+
+        if cursor.cell_num < num_cells:
+            pass
+
+        key_offset = self.leaf_node_key(cursor.cell_num)
+        value_offset = self.leaf_node_value(cursor.cell_num)
+
+        self.page = self.page[key_offset:value_offset] = key.to_bytes(1, "big") + row
--- toysql/pager.py
+++ toysql/pager.py
@@ -1,16 +1,29 @@
-from typing import Protocol
+from typing import Protocol, Any
 from pathlib import Path
+from toysql.btree import Node
 import os
 
 
 class Pager:
+    """
+    Abstracts the filesystem into pages.
+    Everything is stored into a file. This allows you to
+    get and set pages (chunks) of data.
+    """
+
     def __init__(self, file_path: str, page_size=4096):
         file_name = Path(file_path)
         file_name.touch(exist_ok=True)
         self.f = open(file_name, "rb+")
         self.page_size = page_size
+        self.num_pages = int(self.__sizeof__() / page_size)
+
+        # TODO check for corrupt file.
+        # file_length % PAGE_SIZE != 0
 
     def __getitem__(self, i: int):
+        if i >= self.num_pages:
+            self.num_pages += 1
         self.f.seek(i * self.page_size)
         page = bytearray(self.f.read(self.page_size))
         return page
@@ -30,22 +43,41 @@ class Pager:
 
 
 class TableLike(Protocol):
-    row_count: int
+    root_page_num: int
+    pager: Any
 
 
 class Cursor:
-    ## TODO switch this to an iterator.
     def __init__(self, table: TableLike):
         self.table = table
-        self.row_num = 0
+        self.page_num = self.table.root_page_num
+        self.cell_num = 0
+        self.end_of_table = False
+        self.table_start()
 
-    def table_end(self):
-        self.row_num = self.table.row_count
+    def table_start(self):
+        node = self.get_node(self.table.root_page_num)
+        self.cell_num = node.leaf_node_num_cells()
+        self.end_of_table = self.cell_num == 0
         return self
 
-    def advance(self):
-        self.row_num += 1
+    def table_end(self):
+        node = self.get_node(self.table.root_page_num)
+        self.cell_num = node.leaf_node_num_cells()
+        self.end_of_table = True
+        return self
 
-    @property
-    def end_of_table(self):
-        return self.row_num >= self.table.row_count
+    def value(self):
+        node = self.get_node(self.page_num)
+        return node.leaf_node_value(self.cell_num)
+
+    def get_node(self, page_num):
+        page = self.table.pager(page_num)
+        return Node(page)
+
+    def advance(self):
+        node = self.get_node(self.page_num)
+        self.cell_num += 1
+
+        if self.cell_num >= node.leaf_node_num_cells():
+            self.end_of_table = True
