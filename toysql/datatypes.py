1b761c8251548e737741fdb1301b32a73d6f590e
Craig Mulligan <hey@hobochild.com>
2022-04-10 18:37:03 +0100

    add datatypes

---
+++ toysql/datatypes.py
@@ -0,0 +1,28 @@
+from typing import Literal
+from dataclasses import dataclass
+
+ByteOrder = Literal["little", "big"]
+
+
+@dataclass
+class Integer:
+    length: int = 4
+    byteorder: ByteOrder = "little"
+
+    def serialize(self, value) -> bytearray:
+        return bytearray(value.to_bytes(self.length, self.byteorder))
+
+    def deserialize(self, value) -> int:
+        return int.from_bytes(value, self.byteorder)
+
+
+@dataclass
+class String:
+    length: int
+    byteorder: ByteOrder = "little"
+
+    def serialize(self, value) -> bytearray:
+        return bytearray(value.encode("utf-8").ljust(self.length, b"\0"))
+
+    def deserialize(_self, value) -> str:
+        return value.decode("utf-8").rstrip("\x00")
