ff3f5888dce1180c5201ced6998884609d00f6fc
Craig Mulligan <hey@hobochild.com>
2022-04-14 17:11:04 +0100

    switch to in-memory tree

---
+++ toysql/tree.py
@@ -0,0 +1,168 @@
+from toysql.exceptions import DuplicateKeyException
+
+
+class Node(object):
+    """Base node object.
+    Each node stores keys and values.
+    Attributes:
+        order (int): The maximum number of keys each node can hold.
+    """
+
+    def __init__(self, order):
+        """Child nodes can be converted into parent nodes by setting self.leaf = False. Parent nodes
+        simply act as a medium to traverse the tree."""
+        self.order = order
+        self.keys = []
+        self.values = []
+        self.leaf = True
+
+    def add(self, key, value):
+        """Adds a key-value pair to the node."""
+        # If the node is empty, simply insert the key-value pair.
+        if not self.keys:
+            self.keys.append(key)
+            self.values.append(value)
+            return None
+
+        for i, item in enumerate(self.keys.copy()):
+            # If new key matches existing key raise exception.
+            if key == item:
+                raise DuplicateKeyException(f"{i} key already exists")
+
+            # If new key is smaller than existing key, insert new key to the left of existing key.
+            elif key < item:
+                self.keys = self.keys[:i] + [key] + self.keys[i:]
+                self.values = self.values[:i] + [value] + self.values[i:]
+                break
+
+            # If new key is larger than all existing keys, insert new key to the right of all
+            # existing keys.
+            elif i + 1 == len(self.keys):
+                self.keys.append(key)
+                self.values.append(value)
+
+    def split(self):
+        """Splits the node into two and stores them as child nodes."""
+        left = Node(self.order)
+        right = Node(self.order)
+        # // is floor division.
+        mid = self.order // 2
+
+        left.keys = self.keys[:mid]
+        left.values = self.values[:mid]
+
+        right.keys = self.keys[mid:]
+        right.values = self.values[mid:]
+
+        # When the node is split, set the parent key to the left-most key of the right child node.
+        self.keys = [right.keys[0]]
+        self.values = [left, right]
+        self.leaf = False
+
+    def is_full(self):
+        """Returns True if the node is full."""
+        return len(self.keys) == self.order
+
+    def show(self, counter=0):
+        """Prints the keys at each level."""
+        print(counter, str(self.keys))
+
+        # Recursively print the key of child nodes (if these exist).
+        if not self.leaf:
+            for item in self.values:
+                item.show(counter + 1)
+
+    def traverse(self, rows):
+        if not self.leaf:
+            for item in self.values:
+                item.traverse(rows)
+
+        if self.leaf:
+            for i, key in enumerate(self.keys):
+                rows.append([key, self.values[i]])
+
+        return rows
+
+
+class BPlusTree(object):
+    """B+ tree object, consisting of nodes.
+    Nodes will automatically be split into two once it is full. When a split occurs, a key will
+    'float' upwards and be inserted into the parent node to act as a pivot.
+    Attributes:
+        order (int): The maximum number of keys each node can hold.
+    """
+
+    def __init__(self, order=8):
+        self.root = Node(order)
+
+    def _find(self, node, key):
+        """For a given node and key, returns the index where the key should be inserted and the
+        list of values at that index."""
+        for i, item in enumerate(node.keys):
+            if key < item:
+                return node.values[i], i
+
+        return node.values[i + 1], i + 1
+
+    def _merge(self, parent, child, index):
+        """For a parent and child node, extract a pivot from the child to be inserted into the keys
+        of the parent. Insert the values from the child into the values of the parent.
+        """
+        parent.values.pop(index)
+        pivot = child.keys[0]
+
+        for i, item in enumerate(parent.keys):
+            if pivot < item:
+                parent.keys = parent.keys[:i] + [pivot] + parent.keys[i:]
+                parent.values = parent.values[:i] + child.values + parent.values[i:]
+                break
+
+            elif i + 1 == len(parent.keys):
+                parent.keys += [pivot]
+                parent.values += child.values
+                break
+
+    def insert(self, key, value):
+        """Inserts a key-value pair after traversing to a leaf node. If the leaf node is full, split
+        the leaf node into two.
+        """
+        parent = None
+        child = self.root
+        index = 0
+
+        # Traverse tree until leaf node is reached.
+        while not child.leaf:
+            parent = child
+            child, index = self._find(child, key)
+
+        child.add(key, value)
+
+        # If the leaf node is full, split the leaf node into two.
+        if child.is_full():
+            child.split()
+
+            # Once a leaf node is split, it consists of a internal node and two leaf nodes. These
+            # need to be re-inserted back into the tree.
+            if parent and not parent.is_full():
+                self._merge(parent, child, index)
+
+    def find(self, key):
+        """Returns a value for a given key, and None if the key does not exist."""
+        child = self.root
+
+        while not child.leaf:
+            child, index = self._find(child, key)
+
+        for i, item in enumerate(child.keys):
+            if key == item:
+                return child.values[i]
+
+        return None
+
+    def traverse(self):
+        rows = self.root.traverse([])
+        return rows
+
+    def show(self):
+        """Prints the keys at each level."""
+        self.root.show()
