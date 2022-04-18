d27b9b00bd8fdad8dd8ccf9732e60d00600af791
Craig Mulligan <hey@hobochild.com>
2022-04-18 13:41:58 +0100

    write page number

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -26,22 +26,20 @@ def test_to_from_bytes(table):
         row = (i, f"fred-{i}", f"fred-{i}@flintstone.com")
         tree.insert(i, row)
 
-    # leaf_node = tree.root.values[0]
-    # v = tree.root.values[0].to_bytes()
+    leaf_node = tree.root.values[0]
+    v = tree.root.values[0].to_bytes()
 
-    # node = Node(8, table)
-    # node.from_bytes(v)
-    # assert node.leaf == leaf_node.leaf
-    # assert node.keys == leaf_node.keys
-    # assert node.values == leaf_node.values
+    node = Node(8, table)
+    node.from_bytes(v)
+    assert node.leaf == leaf_node.leaf
+    assert node.keys == leaf_node.keys
+    assert node.values == leaf_node.values
 
     root_node = tree.root
     v = root_node.to_bytes()
 
     node = Node(8, table)
-    print(node.leaf)
-    node = node.from_bytes(v)
-    print(node.leaf)
+    node.from_bytes(v)
 
     assert node.leaf == root_node.leaf
     assert node.keys == root_node.keys
