c83e7c68c3ae89b3039708548097b8b921e7b59f
Craig Mulligan <hey@hobochild.com>
2022-04-21 18:30:25 +0100

    add statements

--- toysql/statement.py
+++ toysql/statement.py
@@ -1,5 +1,29 @@
-from typing import Tuple
+from typing import Tuple, Optional, Any
 from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class Op(Enum):
+    GreaterThan = auto()
+    LessThan = auto()
+    Equal = auto()
+    NotEqual = auto()
+
+
+class Expression:
+    # TODO we will need expressions without left right.
+    a: Any
+    b: Any
+    op: Op
+
+
+class KeywordKind(Enum):
+    SymbolKind = auto()
+    IdentifierKind = auto()
+    StringKind = auto()
+    NumericKind = auto()
+    BoolKind = auto()
+    NullKind = auto()
 
 
 class Statement:
@@ -7,8 +31,17 @@ class Statement:
 
 
 @dataclass
+class Token:
+    keyword: str
+    keywork_kind: str
+
+
+@dataclass
 class SelectStatement(Statement):
-    pass
+    _from: Optional[Token] = None
+    where: Optional[Expression] = None
+    limit: Optional[Expression] = None
+    offset: Optional[Expression] = None
 
 
 @dataclass
