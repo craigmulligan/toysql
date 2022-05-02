aa95f50f42364ff1745b3d42d4ceb861a54f45bd
Craig Mulligan <hey@hobochild.com>
2022-05-02 09:37:35 +0100

    use temp dir

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,17 +1,20 @@
 from unittest import TestCase
 from toysql.vm import VM
-import os
+import tempfile
 
 
 class Fixtures(TestCase):
     db_file_path: str
 
     def setUp(self) -> None:
-        self.db_file_path = "__testdb__.db"
-        if os.path.exists(self.db_file_path):
-            os.remove(self.db_file_path)
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.db_file_path = self.temp_dir.name + "/__testdb__.db"
+
         self.vm = VM(self.db_file_path)
         self.pager = self.vm.pager
         self.table = self.vm.table
 
         return super().setUp()
+
+    def cleanUp(self) -> None:
+        self.temp_dir.cleanup()
