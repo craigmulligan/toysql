e171c95809011e4ee8f1126b6af52d78611b8d7d
Craig Mulligan <hey@hobochild.com>
2022-05-20 19:04:36 +0100

    good place

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -17,66 +17,27 @@ from unittest import TestCase, skip
 class TestSymbolLexer(TestCase):
     def test_lex(self):
         lexer = SymbolLexer()
-        cases = [("*", True, 1), (" *", False, 1), ("select", False, 0)]
+        cases = [("*", "*"), (" *", None), ("select", None)]
 
-        for source, found, pointer in cases:
+        for source, value in cases:
             cursor = Cursor(0, Location(0, 0))
-            tokens, cursor = lexer.lex(source, cursor)
-            assert cursor.pointer == pointer
-            assert bool(tokens) is found
+            token, cursor = lexer.lex(source, cursor)
+
+            if token:
+                assert cursor.pointer == len(source)
+                assert token.value == value
+            else:
+                assert value is None
 
 
 class TestNumericLexer(TestCase):
     def test_lex(self):
         lexer = NumericLexer()
-        cases = [("123", True, 4), (" 123", False, 0), ("select", False, 0)]
-
-        for source, found, pointer in cases:
-            cursor = Cursor(0, Location(0, 0))
-            tokens, cursor = lexer.lex(source, cursor)
-            assert cursor.pointer == pointer
-            assert bool(tokens) is found
-
-
-class TestStringLexer(TestCase):
-    def test_lex(self):
-        lexer = StringLexer()
-        cases = [("'abc'", "abc", 5), (" 'abc'", None, 0), ("select", None, 0)]
-
-        for source, value, pointer in cases:
-            cursor = Cursor(0, Location(0, 0))
-            token, cursor = lexer.lex(source, cursor)
-            if token:
-                assert token.value == value
-            else:
-                assert value is None
-
-            assert cursor.pointer == pointer
-
-
-class TestKeywordLexer(TestCase):
-    def test_lex(self):
-        lexer = KeywordLexer()
-        cases = [("select", "select", 6), (" select", None, 0), ("hello", None, 0)]
-
-        for source, value, pointer in cases:
-            cursor = Cursor(0, Location(0, 0))
-            token, cursor = lexer.lex(source, cursor)
-            if token:
-                assert token.value == value
-            else:
-                assert value is None
-
-            assert cursor.pointer == pointer
-
-
-class TestIdentifierLexer(TestCase):
-    def test_lex(self):
-        lexer = IdentifierLexer()
         cases = [
-            ("my_table", "my_table", 8),
-            ('"hello"', "hello", 7),
-            ("12345", None, 0),
+            ("123", "123", 3),
+            ("123 ", "123", 3),
+            (" 123", None, 0),
+            ("select", None, 0),
         ]
 
         for source, value, pointer in cases:
@@ -84,10 +45,58 @@ class TestIdentifierLexer(TestCase):
             token, cursor = lexer.lex(source, cursor)
             if token:
                 assert token.value == value
+                assert cursor.pointer == pointer
             else:
                 assert value is None
 
-            assert cursor.pointer == pointer
+
+class TestStringLexer(TestCase):
+    def test_lex(self):
+        lexer = StringLexer()
+        cases = [("'abc'", "abc"), (" 'abc'", None), ("select", None)]
+
+        for source, value in cases:
+            cursor = Cursor(0, Location(0, 0))
+            token, cursor = lexer.lex(source, cursor)
+            if token:
+                assert token.value == value
+                assert cursor.pointer == len(source)
+            else:
+                assert value is None
+
+
+class TestKeywordLexer(TestCase):
+    def test_lex(self):
+        lexer = KeywordLexer()
+        cases = [("select", "select"), (" select", None), ("hello", None)]
+
+        for source, value in cases:
+            cursor = Cursor(0, Location(0, 0))
+            token, cursor = lexer.lex(source, cursor)
+            if token:
+                assert token.value == value
+                assert cursor.pointer == len(value)
+            else:
+                assert value is None
+
+
+class TestIdentifierLexer(TestCase):
+    def test_lex(self):
+        lexer = IdentifierLexer()
+        cases = [
+            ("my_table", "my_table"),
+            ('"hello"', "hello"),
+            ("12345", None),
+        ]
+
+        for source, value in cases:
+            cursor = Cursor(0, Location(0, 0))
+            token, cursor = lexer.lex(source, cursor)
+            if token:
+                assert token.value == value
+                assert cursor.pointer == len(source)
+            else:
+                assert value is None
 
 
 class TestStatementLexer(TestCase):
@@ -95,7 +104,7 @@ class TestStatementLexer(TestCase):
     def get_token_by_kind(tokens: List[Token], kind: Kind):
         return (token for token in tokens if token.kind == kind)
 
-    @skip("todo")
+    # @skip("oops")
     def test_select(self):
         query = """
             select * from "my_table"
