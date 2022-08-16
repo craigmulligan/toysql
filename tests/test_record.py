2c4eb714d98dc4571113ab6d60a96e7fc35a1b68
Craig Mulligan <git@craigmulligan.com>
2022-08-16 22:38:26 +0200

    redo varints

--- tests/test_record.py
+++ tests/test_record.py
@@ -17,12 +17,25 @@ class TestRecord(TestCase):
 
 class TestInteger(TestCase):
     def test_varint(self):
-        for value, size in [(99, 1), (201, 2), (2147483647, 4)]:
+        for value, size in [(99, 1), (201, 2), (2147483647, 5)]:
             d = Integer(value)
             assert d.value == value 
             raw_bytes = d.to_bytes()
             assert Integer.from_bytes(raw_bytes).value == value 
             assert len(d.to_bytes()) == size
+
+    def test_varint_offset(self):
+        """
+        We we are decoding we likely don't know how many bites to read.
+        When reading we should set the correct offset.
+        """
+        buf = b'\xff\xff\xff\xff\x07\x04\xc9\x01'
+        buf_values = [2147483647, 4, 201]
+
+        for value in buf_values:
+            result = Integer.from_bytes(buf)
+            assert result.value == value
+            buf = buf[result.offset:]
 
 
 class TestText(TestCase):
