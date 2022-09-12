044cf5b67afe0469b09d5572fee22dfb295130de
Craig Mulligan <git@craigmulligan.com>
2022-09-12 12:39:18 -0500

    basic planner blueprint

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -1,26 +1,44 @@
-from unittest import TestCase
+from toysql.parser import Parser
+from toysql.lexer import StatementLexer
+from toysql.planner import Planner
+from tests.fixtures import Fixtures
 
 
-class TestPlanner(TestCase):
+class TestPlanner(Fixtures):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.planner = Planner(self.pager)
+        self.lexer = StatementLexer()
+        self.parser = Parser()
+
+        def prepare(input: str):
+            tokens = self.lexer.lex(input)
+            return self.parser.parse(tokens)
+
+        self.prepare = prepare
+
     def test_select(self):
         """
-        sqlite> explain select name from Artist limit 10;
+        select * from artist;
         addr  opcode         p1    p2    p3    p4             p5  comment
         ----  -------------  ----  ----  ----  -------------  --  -------------
-        0     Init           0     9     0                    0   Start at 9
-        1     Integer        10    1     0                    0   r[1]=10; LIMIT counter
-        2     OpenRead       0     3     0     2              0   root=3 iDb=0; Artist
-        3     Rewind         0     8     0                    0
+        0     Init           0     8     0                    0   Start at 8
+        1     OpenRead       0     3     0     2              0   root=3 iDb=0; artist
+        2     Rewind         0     7     0                    0
+        3       Rowid          0     1     0                    0   r[1]=Artist.rowid
         4       Column         0     1     2                    0   r[2]=Artist.Name
-        5       ResultRow      2     1     0                    0   output=r[2]
-        6       DecrJumpZero   1     8     0                    0   if (--r[1])==0 goto 8
-        7     Next           0     4     0                    1
-        8     Halt           0     0     0                    0
-        9     Transaction    0     0     21    0              1   usesStmtJournal=0
-        10    Goto           0     1     0                    0
+        5       ResultRow      1     2     0                    0   output=r[1..2]
+        6     Next           0     3     0                    1
+        7     Halt           0     0     0                    0
+        8     Transaction    0     0     21    0              1   usesStmtJournal=0
+        9     Goto           0     1     0                    0
 
         Given statements we should create a VM instructions to execute the query.
         """
+        stmt = self.prepare("select * from artist;")
+        program = self.planner.plan(stmt)
+        assert len(program.instructions) == 1
 
     def test_create(self):
         """
