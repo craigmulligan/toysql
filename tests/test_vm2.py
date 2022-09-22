4f2d2b1b6d1e90779c7824e101f4e67c95f99a00
Craig Mulligan <git@craigmulligan.com>
2022-09-22 11:24:51 -0500

    update new_row_id to insert record

--- tests/test_vm2.py
+++ tests/test_vm2.py
@@ -1,7 +1,7 @@
 from toysql.vm import VM
 from toysql.vmV1 import VM as VMV1
 from tests.fixtures import Fixtures
-from toysql.compiler import Compiler
+from toysql.compiler import Compiler, SCHEMA_TABLE_NAME
 
 
 class TestVM(Fixtures):
@@ -17,17 +17,23 @@ class TestVM(Fixtures):
 
         self.root_page_number = 1
 
-    # def test_system_schema_table(self):
-    #     """
-    #     When we init the VM we should auto
-    #     create a schema table.
-    #     """
+    def test_create(self):
+        """
+        When we init the VM we should auto
+        create a schema table.
+        """
+        vm = VMV1(self.pager)
+        table_name = "org"
+        create_stmt = f"CREATE TABLE {table_name} (id INT, name TEXT);"
+        program = Compiler(self.pager, vm).compile(create_stmt)
+        [row for row in vm.execute(program)]
 
-    #     assert self.vm.tables[SCHEME_TABLE_NAME]
-    #     assert len(self.vm.tables.keys()) == 2
-    #     [records] = self.vm.execute(f"SELECT * FROM {SCHEME_TABLE_NAME}")
-    #     assert len(records) == 1
-    #     assert records[0][1] == self.table_name
+        program = Compiler(self.pager, vm).compile(f"SELECT * FROM {SCHEMA_TABLE_NAME}")
+        records = [r for r in vm.execute(program)]
+
+        assert len(records) == 2
+        new_row = records[1]
+        assert new_row == [2, table_name, create_stmt, len(self.pager) - 1]
 
     def test_insert_and_select(self):
         vmv1 = VMV1(self.pager)
