c13338fbfe89d3bf0733ca771a54bf80b3270059
Craig Mulligan <git@craigmulligan.com>
2022-10-20 15:02:48 -0500

    fix vm test link

--- tests/test_vm.py
+++ tests/test_vm.py
@@ -1,102 +1,94 @@
-# import random
+from toysql.vm import VM
+from tests.fixtures import Fixtures
+from toysql.compiler import Compiler, SCHEMA_TABLE_NAME
+import random
 
-# from toysql.vm import VM, SCHEME_TABLE_NAME
-# from toysql.exceptions import DuplicateKeyException
-# from tests.fixtures import Fixtures
 
+class TestVM(Fixtures):
+    def setUp(self) -> None:
+        super().setUp()
+        self.table_name = "users"
+        create_stmt = (
+            f"CREATE TABLE {self.table_name} (id INT, name TEXT(32), email TEXT(255));"
+        )
 
-# class TestVM(Fixtures):
-#     def setUp(self) -> None:
-#         super().setUp()
+        self.vm = VM(self.pager)
+        self.compiler = Compiler(self.pager, self.vm)
 
-#         self.vm = VM(self.db_file_path)
-#         self.table_name = "users"
+        def execute(sql: str):
+            program = self.compiler.compile(sql)
+            return [row for row in self.vm.execute(program)]
 
-#         self.vm.execute(
-#             f"CREATE TABLE {self.table_name} (id INT, name TEXT(32), email TEXT(255));"
-#         )
+        self.execute = execute
 
-#         return
+        program = self.compiler.compile(create_stmt)
+        [row for row in self.vm.execute(program)]
 
-#     def test_system_schema_table(self):
-#         """
-#         When we init the VM we should auto
-#         create a schema table.
-#         """
+        self.root_page_number = 1
 
-#         assert self.vm.tables[SCHEME_TABLE_NAME]
-#         assert len(self.vm.tables.keys()) == 2
-#         [records] = self.vm.execute(f"SELECT * FROM {SCHEME_TABLE_NAME}")
-#         assert len(records) == 1
-#         assert records[0][1] == self.table_name
+    def test_create(self):
+        """
+        When we init the VM we should auto
+        create a schema table.
+        """
+        table_name = "org"
+        create_stmt = f"CREATE TABLE {table_name} (id INT, name TEXT);"
+        self.execute(create_stmt)
+        records = self.execute(f"SELECT * FROM {SCHEMA_TABLE_NAME}")
 
-#     def test_insert_and_select(self):
-#         rows = [
-#             [1, "fred", "fred@flintstone.com"],
-#             [2, "pebbles", "pebbles@flintstone.com"],
-#         ]
+        assert len(records) == 3
+        new_row = records[2]
+        assert new_row == [3, table_name, create_stmt, 2]
 
-#         for row in rows:
-#             self.vm.execute(
-#                 f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
-#             )
+    def test_insert_and_select_x(self):
+        rows = [
+            [1, "fred", "fred@flintstone.com"],
+            [2, "pebbles", "pebbles@flintstone.com"],
+        ]
 
-#         [records] = self.vm.execute(f"SELECT * FROM {self.table_name}")
+        for row in rows:
+            self.execute(
+                f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
+            )
 
-#         assert len(records) == len(rows)
-#         for i, record in enumerate(records):
-#             assert record[0] == rows[i][0]
+        records = self.execute(f"SELECT * FROM {self.table_name}")
 
-#     def test_insert_and_select_with_named_columns(self):
-#         """
-#         Given a list of columns a select statement
-#         returns a subset of columns.
-#         """
-#         rows = [
-#             [1, "fred", "fred@flintstone.com"],
-#             [2, "pebbles", "pebbles@flintstone.com"],
-#         ]
+        assert len(records) == len(rows)
+        for i, record in enumerate(records):
+            assert record[0] == rows[i][0]
 
-#         for row in rows:
-#             self.vm.execute(
-#                 f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
-#             )
+    def test_insert_and_select_many(self):
+        keys = [k for k in range(100)]
+        rows = []
 
-#         [records] = self.vm.execute(f"SELECT name, email FROM {self.table_name}")
+        random.shuffle(keys)
 
-#         assert len(records) == len(rows)
+        for key in keys:
+            rows.append([key, f"name-{key}", f"{key}@flintstone.com"])
 
-#         for i, record in enumerate(records):
-#             assert len(record) == 2
-#             name, email = record
-#             _, input_name, input_email = rows[i]
-#             assert input_name == name
-#             assert input_email == email
+        for row in rows:
+            self.execute(
+                f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
+            )
 
-#     def test_vm_duplicate_key(self):
-#         row = (1, "fred", "fred@flintstone.com")
-#         row_2 = (1, "pebbles", "pebbles@flintstone.com")
-#         self.vm.execute(
-#             f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
-#         )
+        records = self.execute(f"SELECT * FROM {self.table_name}")
 
-#         with self.assertRaises(DuplicateKeyException):
-#             self.vm.execute(
-#                 f"INSERT INTO {self.table_name} VALUES ({row_2[0]}, '{row_2[1]}', '{row_2[2]}');"
-#             )
+        assert len(records) == len(keys)
+        keys.sort()
+        for i, record in enumerate(records):
+            assert record[0] == keys[i]
 
-#     def test_multipage_insert_select(self):
-#         keys = [n for n in range(10)]
-#         random.shuffle(keys)
+    # def test_vm_duplicate_key(self):
+    #     row = (1, "fred", "fred@flintstone.com")
+    #     row_2 = (1, "pebbles", "pebbles@flintstone.com")
 
-#         for n in keys:
-#             row = [n, f"fred-{n}", f"fred-{n}@flintstone.com"]
-#             self.vm.execute(
-#                 f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
-#             )
+    #     program = self.compiler.compile(
+    #         f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
+    #     )
+    #     self.vm.execute(program)
 
-#         [records] = self.vm.execute(f"SELECT * FROM {self.table_name}")
-#         keys.sort()
-
-#         assert len(records) == len(keys)
-#         assert [record[0] for record in records] == keys
+    #     with self.assertRaises(Exception):
+    #         program = self.compiler.compile(
+    #             f"INSERT INTO {self.table_name} VALUES ({row_2[0]}, '{row_2[1]}', '{row_2[2]}');"
+    #         )
+    #         self.vm.execute(program)
---
+++ tests/test_vm_old.py
@@ -0,0 +1,102 @@
+# import random
+
+# from toysql.vm import VM, SCHEME_TABLE_NAME
+# from toysql.exceptions import DuplicateKeyException
+# from tests.fixtures import Fixtures
+
+
+# class TestVM(Fixtures):
+#     def setUp(self) -> None:
+#         super().setUp()
+
+#         self.vm = VM(self.db_file_path)
+#         self.table_name = "users"
+
+#         self.vm.execute(
+#             f"CREATE TABLE {self.table_name} (id INT, name TEXT(32), email TEXT(255));"
+#         )
+
+#         return
+
+#     def test_system_schema_table(self):
+#         """
+#         When we init the VM we should auto
+#         create a schema table.
+#         """
+
+#         assert self.vm.tables[SCHEME_TABLE_NAME]
+#         assert len(self.vm.tables.keys()) == 2
+#         [records] = self.vm.execute(f"SELECT * FROM {SCHEME_TABLE_NAME}")
+#         assert len(records) == 1
+#         assert records[0][1] == self.table_name
+
+#     def test_insert_and_select(self):
+#         rows = [
+#             [1, "fred", "fred@flintstone.com"],
+#             [2, "pebbles", "pebbles@flintstone.com"],
+#         ]
+
+#         for row in rows:
+#             self.vm.execute(
+#                 f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
+#             )
+
+#         [records] = self.vm.execute(f"SELECT * FROM {self.table_name}")
+
+#         assert len(records) == len(rows)
+#         for i, record in enumerate(records):
+#             assert record[0] == rows[i][0]
+
+#     def test_insert_and_select_with_named_columns(self):
+#         """
+#         Given a list of columns a select statement
+#         returns a subset of columns.
+#         """
+#         rows = [
+#             [1, "fred", "fred@flintstone.com"],
+#             [2, "pebbles", "pebbles@flintstone.com"],
+#         ]
+
+#         for row in rows:
+#             self.vm.execute(
+#                 f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
+#             )
+
+#         [records] = self.vm.execute(f"SELECT name, email FROM {self.table_name}")
+
+#         assert len(records) == len(rows)
+
+#         for i, record in enumerate(records):
+#             assert len(record) == 2
+#             name, email = record
+#             _, input_name, input_email = rows[i]
+#             assert input_name == name
+#             assert input_email == email
+
+#     def test_vm_duplicate_key(self):
+#         row = (1, "fred", "fred@flintstone.com")
+#         row_2 = (1, "pebbles", "pebbles@flintstone.com")
+#         self.vm.execute(
+#             f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
+#         )
+
+#         with self.assertRaises(DuplicateKeyException):
+#             self.vm.execute(
+#                 f"INSERT INTO {self.table_name} VALUES ({row_2[0]}, '{row_2[1]}', '{row_2[2]}');"
+#             )
+
+#     def test_multipage_insert_select(self):
+#         keys = [n for n in range(10)]
+#         random.shuffle(keys)
+
+#         for n in keys:
+#             row = [n, f"fred-{n}", f"fred-{n}@flintstone.com"]
+#             self.vm.execute(
+#                 f"INSERT INTO {self.table_name} VALUES ({row[0]}, '{row[1]}', '{row[2]}');"
+#             )
+
+#         [records] = self.vm.execute(f"SELECT * FROM {self.table_name}")
+#         keys.sort()
+
+#         assert len(records) == len(keys)
+#         assert [record[0] for record in records] == keys
