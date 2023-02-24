8e80d5ea76fc88e4f5a71486e613e3a9b7e7b410
Craig Mulligan <git@craigmulligan.com>
2023-02-24 14:06:34 +0200

    twerking

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -1,4 +1,19 @@
-from toysql.datatypes import Varint32
+def from_bin(binary: str):
+    chunks = split_into_chunks(binary, 8)
+    res = ""
+
+    for chunk in chunks:
+        first_bit = chunk[1]
+        if first_bit == 0:
+            break
+
+        res += chunk[1:]
+
+    return int(res, 2)
+
+
+def split_into_chunks(v: str, chunk_size: int):
+    return [v[i : i + chunk_size] for i in range(0, len(v), chunk_size)]
 
 
 def test_varint_32():
@@ -8,8 +23,7 @@ def test_varint_32():
         last_chunk_index = size / 8 - 1
         res = ""
         chunk_size = 7
-        chunks = [v[i : i + chunk_size] for i in range(0, len(v), chunk_size)]
-        print(chunks)
+        chunks = split_into_chunks(v, chunk_size)
 
         for i, chunk in enumerate(chunks):
             first_bit = "1"
@@ -21,8 +35,11 @@ def test_varint_32():
 
         return res
 
-    def bin_to_hex(binary: str):
-        return hex(int(binary, 2))
+    def to_bytes(binary: str):
+        return int(binary, 2).to_bytes(len(binary) // 8, byteorder="big")
+
+    def from_bytes(h: bytes):
+        return format(int.from_bytes(h, byteorder="big"), "b")
 
     n = 1000
     s = 16
@@ -32,4 +49,8 @@ def test_varint_32():
     assert v == "00001111101000"
     assert len(x) == len("1000011101101000")
     assert x == "1000011101101000"
-    assert bin_to_hex(x) == "0x8768"
+    bytearr = to_bytes(x)
+    assert bytearr == b"\x87h"
+    assert from_bin(from_bytes(bytearr)) == 1000
+
+    ###
